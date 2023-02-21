3e925b012a888c14ef2e41b42597258ac9070ad0
gigijeskai <luigindaco@gmail.com>
2023-02-21 10:14:46 +0100

    first api call

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-fetch("https://dummyjson.com/quotes?&limit=100")
-  .then((response) => response.json())
-  .then((data) => {
-    const author = data.quotes.map((quote) => quote.author);
-    console.log(author);
-    let nameEmail = document.getElementById("email-name");
-    nameEmail = "";
-    for (let index = 0; index < author.length; index++) {
-      let singleAuthor = author;
-      nameEmail.innerHTML += `<div class='emails'>
-      <div id='left-part-email'>
-      <div class='email-icons'><input type='checkbox'/>
-      <i class='bi bi-star'>
-      </i><i class='bi bi-caret-right'>
-      </i></div><div class='email-name'>${singleAuthor}</div></div>
-      <div class="email-object">Richiesta generica</div>
-      <div class="email-date">11:11</div>
-    </div>`;
-    }
-  })
-  .catch((error) => console.error(error));
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const author = data.quotes.map((quote) => quote.author);
+//     console.log(author);
+//     let nameEmail = document.getElementById("email-name");
+//     nameEmail = "";
+//     for (let index = 0; index < author.length; index++) {
+//       let singleAuthor = author;
+//       nameEmail.innerHTML += `<p>${singleAuthor}</p>`;
+//     }
+//   })
+//   .catch((error) => console.error(error));
+
+async function showCite() {
+  let httpQuotes = await fetch("https://dummyjson.com/quotes?&limit=100");
+
+  let allQuotes = await httpQuotes.json();
+  let arrayQuotes = allQuotes.quotes.map((quote) => quote.author);
+  let emailBody = document.querySelector(".emails");
+  emailBody.innerHTML = "";
+  for (let i = 0; i < arrayQuotes.length; i++) {
+    const quote = arrayQuotes[i];
+    emailBody.innerHTML += `<div class="email">
+    <div class="left-part-email">
+      <div class="email-icons"><input type="checkbox" /><i class="bi bi-star"></i><i class="bi bi-caret-right"></i></div>
+      <div class="email-name">${quote}</div>
+    </div>
+    <!-- chiusura #left-part-email -->
+    <div class="email-object">Richiesta generica</div>
+    <div class="email-date">11:11</div>
+  </div>`;
+  }
+}
+showCite();
