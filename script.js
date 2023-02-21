611f30f7aa24afdc181baaddcd07e4298dea0b45
gigijeskai <luigindaco@gmail.com>
2023-02-21 12:33:29 +0100

    simple commit

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 async function showAuthor() {
   let httpQuotes = await fetch("https://dummyjson.com/quotes?&limit=100");
+  let httpPosts = await fetch("https://dummyjson.com/posts?&limit=100");
+
+  let allPosts = await httpPosts.json();
+  let arrayPost = allPosts.posts.map((post) => post.title);
+
   let allQuotes = await httpQuotes.json();
   let arrayAuthor = allQuotes.quotes.map((quote) => quote.author);
   let arrayQuotes = allQuotes.quotes.map((quote) => quote.quote);
@@ -8,15 +13,37 @@ async function showAuthor() {
   for (let i = 0; i < arrayAuthor.length; i++) {
     const author = arrayAuthor[i];
     const quote = arrayQuotes[i];
+    const post = arrayPost[i];
+
+    randomTime = () => {
+      hrs = Math.round(Math.random() * 24);
+      mins = Math.round(Math.random() * 60);
+      let hFormat = hrs < 10 ? "0" : "";
+      let mFormat = mins < 10 ? "0" : "";
+      let amPm = hrs < 12 ? "AM" : "PM";
+      let is12 = hrs % 12 === 0;
+
+      return amPm === "AM" && !is12
+        ? String(hFormat + hrs + ":" + mFormat + mins + " " + amPm)
+        : "AM" && is12
+        ? String(12 + ":" + mFormat + mins + " " + amPm)
+        : is12
+        ? String(hFormat + hrs + ":" + mFormat + mins + " " + amPm)
+        : String(hFormat + (hrs - 12) + ":" + mFormat + mins + " " + amPm);
+    };
+
+    let resultTime = this.randomTime();
+
     emailBody.innerHTML += `<div class="email">
     <div class="left-part-email">
       <div class="email-icons"><input type="checkbox" /><i class="bi bi-star"></i><i class="bi bi-caret-right"></i></div>
-      <div class="email-name">${author}</div>
+      <div class="email-name bold">${author}</div>
     </div>
     <!-- chiusura #left-part-email -->
-    <div class="email-object">${quote}</div>
-    <div class="email-date">11:11</div>
+    <div class="email-object"><span class='bold'>${post} </span>   -   ${quote}</div>
+    <div class="email-date">${resultTime}</div>
   </div>`;
   }
 }
+
 showAuthor();
